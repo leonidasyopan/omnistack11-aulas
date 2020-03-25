a2b63feea0cb6488aef0e6c8dffe43319a027fda
Leonidas Yopan <leonidasyopan@gmail.com>
2020-03-25 12:14:16 -0300

    tested methods and params

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+/**
+* Rota & /recurso
+*/
+
+/**
+ *  Método HTTP:
+ * 
+ * GET: Buscar/listar uma informação do Back-end
+ * POST: Criar uma informação no Back-end
+ * PUT: Alterar uma informação no Back-end
+ * DELETE: Deletar uma informação no Back-end
+ */
+
+/**
+ * Tipos de parametro
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (ex.: Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
     return response.json({ 
         evento: 'Semana Omnistack 11.0',
-        aluno: 'Leonidas Yopan'
+        aluno: 'Larissa Yopan'
     });
 });
 
